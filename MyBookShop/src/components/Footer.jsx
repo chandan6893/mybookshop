07de2cc84557d2fefe7172b11411be3c65fcb14a
Chandan Kumar <krc6893@gmail.com>
2023-10-18 22:42:55 +0530

    footer

--- MyBookShop/src/components/Footer.jsx
+++ MyBookShop/src/components/Footer.jsx
@@ -1,25 +1,25 @@
-// import React from 'react';
-// import {  Typography, Box } from "@mui/material";
+import React from 'react';
+import {  Typography, Box } from "@mui/material";
 
-// const Footer = () => {
-//   return (
-//     <>
-//       <Box
-//         sx={{
-//           display: "flex",
-//           justifyContent: "center",
-//           alignItems: "center",
-//           backgroundColor: "#1a237e",
-//           width:"100%",
-//           height:"10vh",
-//         }}
-//       >
-//         <Box>
-//           <Typography color={"lightcyan"} variant="h3">develoaped@2023</Typography>
-//         </Box>
-//       </Box>
-//     </>
-//   );
-// }
+const Footer = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#1a237e",
+          width:"100%",
+          height:"10vh",
+        }}
+      >
+        <Box>
+          <Typography color={"lightcyan"} variant="h3">develoaped@2023</Typography>
+        </Box>
+      </Box>
+    </>
+  );
+}
 
-// export default Footer
+export default Footer
